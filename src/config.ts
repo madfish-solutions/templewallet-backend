e6f17a4ecd20fa7091e7c90ab6a40c5a73a2bafc
Andrii Ivaniv <unixvb@gmail.com>
2022-04-06 15:42:25 +0300

    min android version test

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const MIN_IOS_APP_VERSION = '1.0.201';
-export const MIN_ANDROID_APP_VERSION = '1.0.201';
+export const MIN_IOS_APP_VERSION = '1.0.200';
+export const MIN_ANDROID_APP_VERSION = '1.0.200';
