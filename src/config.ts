9956661053d7dd3b2e09c0626d694280ddf264f6
Andrii Ivaniv <unixvb@gmail.com>
2022-09-16 16:07:47 +0300

    min app versions updated

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const MIN_IOS_APP_VERSION = '1.2.319';
-export const MIN_ANDROID_APP_VERSION = '1.2.319';
+export const MIN_IOS_APP_VERSION = '1.5.382';
+export const MIN_ANDROID_APP_VERSION = '1.5.382';
