b9e201ed4666b9d9e8b4ce2baebe13a7fab185ba
Andrii Ivaniv <unixvb@gmail.com>
2022-11-29 13:16:27 +0200

    mobile min versions updated

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const MIN_IOS_APP_VERSION = '1.5.382';
-export const MIN_ANDROID_APP_VERSION = '1.5.382';
+export const MIN_IOS_APP_VERSION = '1.10.445';
+export const MIN_ANDROID_APP_VERSION = '1.10.445';
