ed0cf4597193a17440fb229e44826b9d96d02357
lendihop <mishin.danil2017@gmail.com>
2023-11-17 19:31:35 +0100

    evm tokenlist loading + bitcoin, bitcoin utxo endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
 require('./configure');
 
+import { EvmChain } from '@moralisweb3/common-evm-utils';
+import axios from 'axios';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express, { Request, Response } from 'express';
 import firebaseAdmin from 'firebase-admin';
+import Moralis from 'moralis';
 import { stdSerializers } from 'pino';
 import pinoHttp from 'pino-http';
+import { isString } from 'util';
 
 import { getAdvertisingInfo } from './advertising/advertising';
-import { MIN_ANDROID_APP_VERSION, MIN_IOS_APP_VERSION } from './config';
+import { MIN_ANDROID_APP_VERSION, MIN_IOS_APP_VERSION, EnvVars } from './config';
 import getDAppsStats from './getDAppsStats';
-import { basicAuth } from './middlewares/basic-auth.middleware';
-import { Notification, PlatformType } from './notifications/notification.interface';
-import { getImageFallback } from './notifications/utils/get-image-fallback.util';
-import { getNotifications } from './notifications/utils/get-notifications.util';
-import { getParsedContent } from './notifications/utils/get-parsed-content.util';
-import { getPlatforms } from './notifications/utils/get-platforms.util';
-import { redisClient } from './redis';
+// import { basicAuth } from './middlewares/basic-auth.middleware';
+// import { Notification, PlatformType } from './notifications/notification.interface';
+// import { getImageFallback } from './notifications/utils/get-image-fallback.util';
+// import { getNotifications } from './notifications/utils/get-notifications.util';
+// import { getParsedContent } from './notifications/utils/get-parsed-content.util';
+// import { getPlatforms } from './notifications/utils/get-platforms.util';
+// import { redisClient } from './redis';
 import { getABData } from './utils/ab-test';
 import { cancelAliceBobOrder } from './utils/alice-bob/cancel-alice-bob-order';
 import { createAliceBobOrder } from './utils/alice-bob/create-alice-bob-order';
@@ -26,12 +30,12 @@ import { getAliceBobOrderInfo } from './utils/alice-bob/get-alice-bob-order-info
 import { getAliceBobPairInfo } from './utils/alice-bob/get-alice-bob-pair-info';
 import { getAliceBobPairsInfo } from './utils/alice-bob/get-alice-bob-pairs-info';
 import { coinGeckoTokens } from './utils/gecko-tokens';
-import { getExternalApiErrorPayload, isDefined, isNonEmptyString } from './utils/helpers';
+import { getExternalApiErrorPayload, isDefined } from './utils/helpers';
 import logger from './utils/logger';
 import { getSignedMoonPayUrl } from './utils/moonpay/get-signed-moonpay-url';
 import SingleQueryDataProvider from './utils/SingleQueryDataProvider';
-import { tezExchangeRateProvider } from './utils/tezos';
-import { tokensExchangeRatesProvider } from './utils/tokens';
+//import { tezExchangeRateProvider } from './utils/tezos';
+//import { tokensExchangeRatesProvider } from './utils/tokens';
 
 const PINO_LOGGER = {
   logger: logger.child({ name: 'web' }),
@@ -100,61 +104,61 @@ app.get('/api/top-coins', (_req, res) => {
   res.status(200).send(coinGeckoTokens);
 });
 
-app.get('/api/notifications', async (_req, res) => {
-  try {
-    const { platform, startFromTime } = _req.query;
-    const data = await getNotifications(
-      redisClient,
-      platform === PlatformType.Mobile ? PlatformType.Mobile : PlatformType.Extension,
-      Number(startFromTime) ?? 0
-    );
+// app.get('/api/notifications', async (_req, res) => {
+//   try {
+//     const { platform, startFromTime } = _req.query;
+//     const data = await getNotifications(
+//       redisClient,
+//       platform === PlatformType.Mobile ? PlatformType.Mobile : PlatformType.Extension,
+//       Number(startFromTime) ?? 0
+//     );
+//
+//     res.status(200).send(data);
+//   } catch (error) {
+//     res.status(500).send({ error });
+//   }
+// });
 
-    res.status(200).send(data);
-  } catch (error) {
-    res.status(500).send({ error });
-  }
-});
-
-app.post('/api/notifications', basicAuth, async (req, res) => {
-  try {
-    const {
-      mobile,
-      extension,
-      type,
-      title,
-      description,
-      extensionImageUrl,
-      mobileImageUrl,
-      content,
-      date,
-      expirationDate,
-      isMandatory
-    } = req.body;
-
-    const newNotification: Notification = {
-      id: Date.now(),
-      createdAt: date,
-      type,
-      platforms: getPlatforms(mobile, extension),
-      language: 'en-US',
-      title,
-      description,
-      content: getParsedContent(content),
-      extensionImageUrl: isNonEmptyString(extensionImageUrl)
-        ? extensionImageUrl
-        : getImageFallback(PlatformType.Extension, type),
-      mobileImageUrl: isNonEmptyString(mobileImageUrl) ? mobileImageUrl : getImageFallback(PlatformType.Mobile, type),
-      expirationDate,
-      isMandatory: isDefined(isMandatory)
-    };
-
-    await redisClient.lpush('notifications', JSON.stringify(newNotification));
-
-    res.status(200).send({ message: 'Notification added successfully' });
-  } catch (error: any) {
-    res.status(500).send({ error: error.message });
-  }
-});
+// app.post('/api/notifications', basicAuth, async (req, res) => {
+//   try {
+//     const {
+//       mobile,
+//       extension,
+//       type,
+//       title,
+//       description,
+//       extensionImageUrl,
+//       mobileImageUrl,
+//       content,
+//       date,
+//       expirationDate,
+//       isMandatory
+//     } = req.body;
+//
+//     const newNotification: Notification = {
+//       id: Date.now(),
+//       createdAt: date,
+//       type,
+//       platforms: getPlatforms(mobile, extension),
+//       language: 'en-US',
+//       title,
+//       description,
+//       content: getParsedContent(content),
+//       extensionImageUrl: isNonEmptyString(extensionImageUrl)
+//         ? extensionImageUrl
+//         : getImageFallback(PlatformType.Extension, type),
+//       mobileImageUrl: isNonEmptyString(mobileImageUrl) ? mobileImageUrl : getImageFallback(PlatformType.Mobile, type),
+//       expirationDate,
+//       isMandatory: isDefined(isMandatory)
+//     };
+//
+//     await redisClient.lpush('notifications', JSON.stringify(newNotification));
+//
+//     res.status(200).send({ message: 'Notification added successfully' });
+//   } catch (error: any) {
+//     res.status(500).send({ error: error.message });
+//   }
+// });
 
 app.get('/api/dapps', makeProviderDataRequestHandler(dAppsProvider));
 
@@ -163,32 +167,32 @@ app.get('/api/abtest', (_, res) => {
   res.json(data);
 });
 
-app.get('/api/exchange-rates/tez', makeProviderDataRequestHandler(tezExchangeRateProvider));
+//app.get('/api/exchange-rates/tez', makeProviderDataRequestHandler(tezExchangeRateProvider));
 
-app.get('/api/exchange-rates', async (_req, res) => {
-  const { data: tokensExchangeRates, error: tokensExchangeRatesError } = await getProviderStateWithTimeout(
-    tokensExchangeRatesProvider
-  );
-  const { data: tezExchangeRate, error: tezExchangeRateError } = await getProviderStateWithTimeout(
-    tezExchangeRateProvider
-  );
-  if (tokensExchangeRatesError !== undefined) {
-    return res.status(500).send({
-      error: tokensExchangeRatesError.message
-    });
-  } else if (tezExchangeRateError !== undefined) {
-    return res.status(500).send({
-      error: tezExchangeRateError.message
-    });
-  } else {
-    if (tokensExchangeRates !== undefined && tezExchangeRate !== undefined) {
-      return res.json([
-        ...tokensExchangeRates.map(({ ...restProps }) => restProps),
-        { exchangeRate: tezExchangeRate.toString() }
-      ]);
-    }
-  }
-});
+// app.get('/api/exchange-rates', async (_req, res) => {
+//   const { data: tokensExchangeRates, error: tokensExchangeRatesError } = await getProviderStateWithTimeout(
+//     tokensExchangeRatesProvider
+//   );
+//   const { data: tezExchangeRate, error: tezExchangeRateError } = await getProviderStateWithTimeout(
+//     tezExchangeRateProvider
+//   );
+//   if (tokensExchangeRatesError !== undefined) {
+//     return res.status(500).send({
+//       error: tokensExchangeRatesError.message
+//     });
+//   } else if (tezExchangeRateError !== undefined) {
+//     return res.status(500).send({
+//       error: tezExchangeRateError.message
+//     });
+//   } else {
+//     if (tokensExchangeRates !== undefined && tezExchangeRate !== undefined) {
+//       return res.json([
+//         ...tokensExchangeRates.map(({ ...restProps }) => restProps),
+//         { exchangeRate: tezExchangeRate.toString() }
+//       ]);
+//     }
+//   }
+// });
 
 app.get('/api/moonpay-sign', async (_req, res) => {
   try {
@@ -332,6 +336,241 @@ app.get('/api/advertising-info', (_req, res) => {
   }
 });
 
+app.get('/api/bitcoin', async (_req, res) => {
+  const { addresses: addressesConcat } = _req.query;
+  console.log(addressesConcat);
+
+  if (!isString(addressesConcat)) {
+    throw new Error('Addresses is not a string');
+  }
+
+  try {
+    const addresses = addressesConcat.split(';');
+
+    const utxoPromises = addresses.map(async address => {
+      try {
+        const response = await axios.get(`https://blockstream.info/testnet/api/address/${address}/utxo`);
+
+        return { address, utxos: response.data };
+      } catch (error: any) {
+        console.error(`Error fetching utxos for ${address}: ${error.message}`);
+
+        return { address, utxos: [] };
+      }
+    });
+
+    const utxos = await Promise.all(utxoPromises);
+
+    const allUtxos = utxos.flatMap(({ utxos }) => utxos);
+
+    const totalAmountAvailable: number = allUtxos.reduce((acc, utxo) => (acc += utxo.value), 0);
+
+    const bitcoin = {
+      token_address: 'btc',
+      symbol: 'BTC',
+      name: 'Bitcoin',
+      logo: 'https://cdn.moralis.io/eth/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599.png',
+      thumbnail: 'https://cdn.moralis.io/eth/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599_thumb.png',
+      decimals: 8,
+      balance: totalAmountAvailable.toFixed(),
+      chainName: 'Bitcoin Testnet',
+      nativeToken: true,
+      possible_spam: false
+    };
+
+    res.status(200).send(bitcoin);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+app.get('/api/bitcoin-utxos', async (_req, res) => {
+  const { addresses: addressesConcat } = _req.query;
+  console.log(addressesConcat);
+
+  if (!isString(addressesConcat)) {
+    throw new Error('Addresses is not a string');
+  }
+
+  try {
+    const addresses = addressesConcat.split(';');
+
+    const utxoPromises = addresses.map(async address => {
+      try {
+        const response = await axios.get(`https://blockstream.info/testnet/api/address/${address}/utxo`);
+
+        return { address, utxos: response.data };
+      } catch (error: any) {
+        console.error(`Error fetching utxos for ${address}: ${error.message}`);
+
+        return { address, utxos: [] };
+      }
+    });
+
+    const utxos = await Promise.all(utxoPromises);
+
+    res.status(200).send(utxos);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+app.post('/api/bitcoin-broadcast-tx', async (_req, res) => {
+  const { txHex } = _req.query;
+  console.log(txHex);
+
+  if (!isString(txHex)) {
+    throw new Error('txHex is not a string');
+  }
+
+  try {
+    const { data } = await axios.post('https://api.blockcypher.com/v1/btc/test3/txs/push', {
+      tx: txHex
+    });
+
+    console.log('Transaction broadcast successful:', data);
+
+    res.status(200).send(data);
+  } catch (error: any) {
+    if (Boolean(error.response)) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error('Transaction broadcast failed with status code:', error.response.status);
+      console.error('Error data:', error.response.data);
+    } else if (Boolean(error.request)) {
+      // The request was made but no response was received
+      console.error('No response received from the server:', error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error('Error setting up the request:', error.message);
+      res.status(500).send({ error: error.response.data });
+    }
+  }
+});
+
+app.get('/api/evm-tokens', async (_req, res) => {
+  const { address } = _req.query;
+
+  const tokens = [
+    {
+      token_address: 'eth',
+      symbol: 'ETH',
+      name: 'Ether',
+      logo: 'https://cdn.moralis.io/eth/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.png',
+      thumbnail: 'https://cdn.moralis.io/eth/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee_thumb.png',
+      decimals: 18,
+      balance: '0',
+      chainName: 'Ethereum Sepolia',
+      nativeToken: true,
+      possible_spam: false
+    },
+    {
+      token_address: 'matic',
+      symbol: 'MATIC',
+      name: 'Matic',
+      logo: 'https://cdn.moralis.io/eth/0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0.png',
+      thumbnail: 'https://cdn.moralis.io/eth/0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0_thumb.png',
+      decimals: 18,
+      balance: '0',
+      chainName: 'Polygon Mumbai',
+      nativeToken: true,
+      possible_spam: false
+    },
+    {
+      token_address: 'bnb',
+      symbol: 'BNB',
+      name: 'BNB',
+      logo: 'https://cdn.moralis.io/eth/0x4fabb145d64652a948d72533023f6e7a623c7c53.png',
+      thumbnail: 'https://cdn.moralis.io/eth/0x4fabb145d64652a948d72533023f6e7a623c7c53_thumb.png',
+      decimals: 18,
+      balance: '0',
+      chainName: 'BSC Testnet',
+      nativeToken: true,
+      possible_spam: false
+    }
+  ];
+
+  try {
+    const ethBalanceResponse = await Moralis.EvmApi.balance.getNativeBalance({
+      address: address as string,
+      chain: EvmChain.SEPOLIA
+    });
+
+    const maticBalanceResponse = await Moralis.EvmApi.balance.getNativeBalance({
+      address: address as string,
+      chain: EvmChain.MUMBAI
+    });
+
+    const bnbBalanceResponse = await Moralis.EvmApi.balance.getNativeBalance({
+      address: address as string,
+      chain: EvmChain.BSC_TESTNET
+    });
+
+    tokens[0].balance = ethBalanceResponse.raw.balance;
+    tokens[1].balance = maticBalanceResponse.raw.balance;
+    tokens[2].balance = bnbBalanceResponse.raw.balance;
+
+    const tokensWithBalances1 = await Moralis.EvmApi.token.getWalletTokenBalances({
+      address: address as string,
+      chain: EvmChain.SEPOLIA
+    });
+    const tokensWithBalances2 = await Moralis.EvmApi.token.getWalletTokenBalances({
+      address: address as string,
+      chain: EvmChain.BSC_TESTNET
+    });
+
+    const tokensWithBalances = tokensWithBalances1.raw
+      .map(token => ({ ...token, chainName: 'Ethereum Sepolia' }))
+      .concat(tokensWithBalances2.raw.map(token => ({ ...token, chainName: 'BSC Testnet' })));
+
+    //testnet tokens does not have logos
+    const mainnetTokensMetadataResponse = await Moralis.EvmApi.token.getTokenMetadata({
+      addresses: [
+        '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+        '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
+        '0x4fabb145d64652a948d72533023f6e7a623c7c53',
+        '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'
+      ],
+      chain: EvmChain.ETHEREUM
+    });
+
+    const logos = mainnetTokensMetadataResponse.raw.reduce((acc, metadata) => {
+      if (metadata.symbol === 'WBTC') {
+        acc['BTCB'] = { logo: metadata.logo, thumb: metadata.thumbnail };
+
+        return acc;
+      }
+
+      acc[metadata.symbol] = { logo: metadata.logo, thumb: metadata.thumbnail };
+
+      return acc;
+    }, {});
+
+    const tokensWithBalancesAndLogos = tokensWithBalances.map(token => ({
+      ...token,
+      logo: isDefined(logos[token.symbol]) ? logos[token.symbol].logo : '',
+      thumbnail: isDefined(logos[token.symbol]) ? logos[token.symbol].thumb : '',
+      name: token.symbol === 'BTCB' ? 'Binance-Peg BTCB Token' : token.name,
+      nativeToken: false
+    }));
+
+    tokens.push(...tokensWithBalancesAndLogos);
+
+    res.status(200).send(tokens);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
 // start the server listening for requests
 const port = Boolean(process.env.PORT) ? process.env.PORT : 3000;
-app.listen(port, () => console.info(`Server is running on port ${port}...`));
+
+const startServer = async () => {
+  await Moralis.start({
+    apiKey: EnvVars.MORALIS_API_KEY
+  });
+
+  app.listen(port, () => console.info(`Server is running on port ${port}...`));
+};
+
+startServer();
