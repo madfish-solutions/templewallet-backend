c0eaab14d8a1aca398d62dc2dcabf709a2f3bcc4
Alex <alex.seleznov@gmail.com>
2024-10-16 23:13:34 +0300

    TW-1479: [EVM] Transactions history

--- src/routers/evm/index.ts
+++ src/routers/evm/index.ts
@@ -1,44 +1,69 @@
 import { Router } from 'express';
 
-import { withCodedExceptionHandler, withEvmQueryValidation } from '../../utils/express-helpers';
-import { getEvmBalances, getEvmCollectiblesMetadata, getEvmTokensMetadata, getStringifiedResponse } from './covalent';
+import { withCodedExceptionHandler } from '../../utils/express-helpers';
+import {
+  evmQueryParamsSchema,
+  evmQueryParamsPaginatedSchema,
+  evmQueryParamsTransfersSchema
+} from '../../utils/schemas';
+import {
+  getEvmBalances,
+  getEvmCollectiblesMetadata,
+  getEvmTokensMetadata,
+  getEvmAccountTransactions,
+  getEvmAccountERC20Transfers
+} from './covalent';
 
 export const evmRouter = Router();
 
 evmRouter
   .get(
     '/balances',
-    withCodedExceptionHandler(
-      withEvmQueryValidation(async (_1, res, _2, evmQueryParams) => {
-        const { walletAddress, chainId } = evmQueryParams;
+    withCodedExceptionHandler(async (req, res) => {
+      const { walletAddress, chainId } = await evmQueryParamsSchema.validate(req.query);
 
-        const data = await getEvmBalances(walletAddress, chainId);
+      const data = await getEvmBalances(walletAddress, chainId);
 
-        res.status(200).send(getStringifiedResponse(data));
-      })
-    )
+      res.status(200).send(data);
+    })
   )
   .get(
     '/tokens-metadata',
-    withCodedExceptionHandler(
-      withEvmQueryValidation(async (_1, res, _2, evmQueryParams) => {
-        const { walletAddress, chainId } = evmQueryParams;
+    withCodedExceptionHandler(async (req, res) => {
+      const { walletAddress, chainId } = await evmQueryParamsSchema.validate(req.query);
 
-        const data = await getEvmTokensMetadata(walletAddress, chainId);
+      const data = await getEvmTokensMetadata(walletAddress, chainId);
 
-        res.status(200).send(getStringifiedResponse(data));
-      })
-    )
+      res.status(200).send(data);
+    })
   )
   .get(
     '/collectibles-metadata',
-    withCodedExceptionHandler(
-      withEvmQueryValidation(async (_1, res, _2, evmQueryParams) => {
-        const { walletAddress, chainId } = evmQueryParams;
+    withCodedExceptionHandler(async (req, res) => {
+      const { walletAddress, chainId } = await evmQueryParamsSchema.validate(req.query);
 
-        const data = await getEvmCollectiblesMetadata(walletAddress, chainId);
+      const data = await getEvmCollectiblesMetadata(walletAddress, chainId);
 
-        res.status(200).send(getStringifiedResponse(data));
-      })
-    )
+      res.status(200).send(data);
+    })
+  )
+  .get(
+    '/transactions',
+    withCodedExceptionHandler(async (req, res) => {
+      const { walletAddress, chainId, page } = await evmQueryParamsPaginatedSchema.validate(req.query);
+
+      const data = await getEvmAccountTransactions(walletAddress, chainId, page);
+
+      res.status(200).send(data);
+    })
+  )
+  .get(
+    '/erc20-transfers',
+    withCodedExceptionHandler(async (req, res) => {
+      const { walletAddress, chainId, contractAddress, page } = await evmQueryParamsTransfersSchema.validate(req.query);
+
+      const data = await getEvmAccountERC20Transfers(walletAddress, chainId, contractAddress, page);
+
+      res.status(200).send(data);
+    })
   );
