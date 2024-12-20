999f45ad058f0f4a5300982a455551206fce8fc9
Danyl Mishyn <35381314+lendihop@users.noreply.github.com>
2024-12-20 12:47:28 +0100

    TW-1579: Exolix networks map (#181)

---
+++ src/utils/exolix-networks-map.ts
@@ -0,0 +1,14 @@
+export const exolixNetworksMap: Record<number, string> = {
+  1: 'ETH',
+  56: 'BSC',
+  10: 'OPTIMISM',
+  43114: 'AVAXC',
+  42161: 'ARBITRUM',
+  8453: 'BASE',
+  314: 'FIL',
+  250: 'FTM',
+  2222: 'KAVA',
+  88888: 'CHZ',
+  42220: 'CELO',
+  1666600000: 'ONE'
+};
