338a3b74fd1cd3342f415bedf40cbcf51be82b1d
Inokentii Mazhara <keshan3262@gmail.com>
2025-08-19 16:53:49 +0300

    Revert "Add memoization for redirection"
    This reverts commit f18225b69e15354cd0108ffe97dc6438b3542cc2.

--- src/utils/temple-ads-redirection.ts
+++ src/utils/temple-ads-redirection.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import memoizee from 'memoizee';
 
 import { EnvVars } from '../config';
 
@@ -9,18 +8,14 @@ export const addTempleAdsRedirection = (
   isMisesVariable: boolean,
   templeAdsPathMainPart = path
 ) => {
-  const getRedirectionUrl = memoizee(
-    (extVersion: string, isMisesBrowser: boolean) =>
-      `${EnvVars.TEMPLE_ADS_API_URL}/v1/api/ads-rules/${extVersion}${templeAdsPathMainPart}${
-        isMisesVariable ? (isMisesBrowser ? '/mises' : '/no-mises') : ''
-      }`,
-    { max: 1000, normalizer: args => JSON.stringify(args) }
-  );
-
   router.get(path, (req, res) => {
     const { extVersion: rawExtVersion, isMisesBrowser: rawIsMisesBrowser } = req.query;
     const extVersion = typeof rawExtVersion === 'string' ? rawExtVersion : '0.0.0';
     const isMisesBrowser = rawIsMisesBrowser === 'true';
-    res.redirect(getRedirectionUrl(extVersion, isMisesBrowser));
+    res.redirect(
+      `${EnvVars.TEMPLE_ADS_API_URL}/v1/api/ads-rules/${extVersion}${templeAdsPathMainPart}${
+        isMisesVariable ? (isMisesBrowser ? '/mises' : '/no-mises') : ''
+      }`
+    );
   });
 };
